722901af5224da4cb3acef0169086810fb383482
alexrodriguez <alexcarvajal2404@gmail.com>
2024-10-03 14:08:57 -0500

    unused vars

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
-import {Menu} from "lucide-react";
 import MenuButton from "@/app/(web)/MenuButton";
 
 const navbarItems = [
